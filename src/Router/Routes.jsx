04ca0eb29a562e37f997ef0792e7834b234c7dd6
suvo <suvodatta72@gamil.com>
2025-03-13 12:27:45 +0600

    signUpForm desing done

--- src/Router/Routes.jsx
+++ src/Router/Routes.jsx
@@ -3,12 +3,14 @@ import { Routes, Route } from "react-router";
 import Home from "../Pages/Home/Home";
 import MainLayout from "../Layouts/MainLayout";
 import Error from "../Pages/Error/Error";
+import SignupForm from "../Pages/SingUp/SignupForm";
 
 const Routers = () => {
   return (
     <Routes>
       <Route path="/" element={<MainLayout />}>
         <Route index element={<Home />} />
+        <Route path="singup" element={<SignupForm />} />
       </Route>
       <Route path="*" element={<Error />} />
     </Routes>
